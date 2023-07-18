99c744d0cafebbdeba8c66c48ac847fad895bada
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-07-18 19:01:42 -0300

    18/07
    largando a primeira versão

--- js.js
+++ js.js
@@ -6,6 +6,13 @@ let estado = 'normal'
 let segu = null
 const btn_login = document.getElementById('btn_login')
 const btn_cadastro = document.getElementById('btn_cadastro')
+const tela = document.getElementById('tela')
+const subtela= document.getElementById('subtela')
+const inome = document.getElementById('inome')
+const isenha = document.getElementById('isenha')
+const btn_enviar = document.getElementById('btn_enviar')
+const nav = document.getElementById('nav')
+
 
 
 
@@ -29,11 +36,28 @@ const seg = ()=>{
     segu= 'nao'
     console.log(segu)
     dv1.classList.remove('hoverout')
+    dv1.classList.remove('hover')
     dv2.classList.remove('hoverout')
+    dv2.classList.remove('hover')
     setTimeout(()=>{
         segu= null
         console.log(segu)
     },2100)
+}
+
+const mudarh1=()=>{
+    setTimeout(()=>{
+        tela.innerHTML= 'Seja bem vindo novamente'
+        subtela.innerHTML= 'Insira as informações do seu login'
+        btn.style.display= 'none'
+        btn_enviar.style.display= 'block'
+        nav.classList.add('nav')
+    },1000)
+
+    setTimeout(()=>{
+        nav.style.marginTop= '80px'
+    },200)
+   
 }
 
 btn.addEventListener('click' , ()=>{
@@ -44,6 +68,7 @@ btn.addEventListener('click' , ()=>{
         inverter()
         dv1.classList.remove('hoverout')
         dv2.classList.remove('hoverout')
+        mudarh1()
     }else if(estado == 'trocado'){
         dv1.classList.add('dv2a')
         dv2.classList.add('dv1a')
@@ -56,7 +81,7 @@ btn.addEventListener('click' , ()=>{
 dv1.addEventListener('mouseover' , ()=>{
     if(segu === null){
     dv1.classList.add('hover')
-    btn_login.classList.add('btna')
+    btn.classList.add('btna')
     btn_cadastro.classList.add('btna')
     }
 })
@@ -65,7 +90,7 @@ dv1.addEventListener('mouseout' , ()=>{
     if(segu === null){
     dv1.classList.add('hoverout')
     dv1.classList.remove('hover')
-    btn_login.classList.remove('btna')
+    btn.classList.remove('btna')
     btn_cadastro.classList.remove('btna')
     }
 })
@@ -73,6 +98,9 @@ dv1.addEventListener('mouseout' , ()=>{
 dv2.addEventListener('mouseover' , ()=>{
     if(segu === null){
     dv2.classList.add('hover')
+    inome.classList.add('pequeno')
+    isenha.classList.add('pequeno')
+    nav.style.marginTop= '40px'
     }
 })
 
@@ -80,5 +108,8 @@ dv2.addEventListener('mouseout' , ()=>{
     if(segu === null){
     dv2.classList.add('hoverout')
     dv2.classList.remove('hover')
+    inome.classList.remove('pequeno')
+    isenha.classList.remove('pequeno')
+    nav.style.marginTop= '40px'
     }
 })
